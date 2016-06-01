e82151891014cc3da827c9f4242426fdf8f6adde
Jingtao Chen <jingtao.ch@gmail.com>
2016-06-01 16:24:07 +0800

    crontab
    Signed-off-by: Jingtao Chen <jingtao.ch@gmail.com>

--- backup_cron.sh
+++ backup_cron.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/bin/bash /home/pi/Backup/backup_typecho.sh /home/wwwroot/www.bitbite.cn 2>&1 1>&"/home/pi/Backup/log-`date-%F%T`.txt"
+/bin/bash /home/pi/Backup/backup_typecho.sh /home/wwwroot/www.bitbite.cn 2>&1 1>&"/home/pi/Backup/log-`date +%F%T`.txt"
